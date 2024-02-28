3f55490c5128b8de7e6892b9a22d8e73a1aa448e
Veronika Potapova <potapova_veronika@lenta.ru>
2024-02-28 18:09:49 +0300

    создаем новый массив размером, равным количеству подсчитанных элементов

--- Program.cs
+++ Program.cs
@@ -22,3 +22,15 @@ string [] symbol(string [] stringArray)
     if(stringArray[i].Length <=3)
     n++;
   }
+    string [] rez = new string [n];
+  int j = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
+}
