125d3ade87a76da7ac63269efef1302e42b439e9
Veronika Potapova <potapova_veronika@lenta.ru>
2024-02-28 18:07:52 +0300

    наполняем массив строками (ввод в консоли)

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@
 Console.Write("Введите количество элементов массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [m];
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     stringArray[i] = Console.ReadLine();
+  }
+}
